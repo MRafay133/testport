b832485db1e0414537a0163880e1b99328f093e0
M.Rafay <M.Rafay@DESKTOP-5A4KMR4>
2025-01-14 14:11:15 +0500

    Add project files.

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using testport.Models;
+
+namespace testport.Data
+{
+    public class AppDbContext : DbContext
+    {
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) 
+        { 
+
+        }
+
+        public DbSet<Login> Login { get; set; }
+    }
+}
---
+++ Models/Login.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace testport.Models
+{
+    public class Login
+    {
+        
+        public string name { get; set; }
+
+        [Key]
+        public string email { get; set; }
+
+        public string message { get; set; }
+    }
+}
